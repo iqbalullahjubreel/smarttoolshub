cf68cd86b0ccaab05cb4d5a2c107d9070335acf2
iqbalullahjubreel <iqbalullahj@gmail.com>
2025-08-05 08:39:20 +0100

    Moved favicon to root and fixed path in index.html

--- obj/Debug/net9.0/Cash App.AssemblyInfo.cs
+++ obj/Debug/net9.0/Cash App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cash App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed48f4d5468c683020e4e906ac8303e18282f52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae26702b09796d61e22e4b16d37e3862387217d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cash App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cash App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
