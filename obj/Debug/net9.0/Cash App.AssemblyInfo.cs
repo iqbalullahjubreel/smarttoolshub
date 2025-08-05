6a61519469e81c3196ecd6dee343ec2103212e16
iqbalullahjubreel <iqbalullahj@gmail.com>
2025-08-05 10:13:38 +0100

    Resolve merge conflicts

--- obj/Debug/net9.0/Cash App.AssemblyInfo.cs
+++ obj/Debug/net9.0/Cash App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cash App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a994fb9655859eaa5e59e672ca59b93bf1b673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cadc1234e704130ffef3d093246a1be2a99ba9bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cash App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cash App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
